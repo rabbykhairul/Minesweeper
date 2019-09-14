4a888391994d2919d808bf19a94be8e02bff5827
rabbykhairul <rabbykhairul@gmail.com>
2019-09-14 16:02:14 +0600

    Add functionality to detect game over condition

--- game.rb
+++ game.rb
@@ -1,7 +1,23 @@
 require_relative "./board.rb"
 
 class Game
+    attr_reader :board
+    attr_accessor :move_position
+
     def initialize
         @board = Board.new
+        @move_position
+    end
+
+    def game_over?
+        stepped_on_a_bomb? || opened_all_bomb_free_tiles?
+    end
+
+    def stepped_on_a_bomb?
+        board.stepped_on_a_bomb?(move_position)
+    end
+
+    def opened_all_bomb_free_tiles?
+        board.openen_all_bomb_free_tiles?
     end
 end
