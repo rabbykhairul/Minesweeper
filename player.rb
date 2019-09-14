5d149b1a33550e290d31e30b3b6c0d31207549dc
rabbykhairul <rabbykhairul@gmail.com>
2019-09-14 17:15:33 +0600

    Will display a prompt to the player asking for input

--- player.rb
+++ player.rb
@@ -8,4 +8,13 @@ class Player
         display_prompt_and_get_command
         extract_move_wish_from_command
     end
+
+    def display_prompt_and_get_command
+        puts "Use `r` to reveal a tile"
+        puts "Use `f` to flag a tile"
+        puts "Use `u` to unflag a flagged tile"
+        puts
+        print "Enter tile position (e.g: r2,3): "
+        receive_user_input_and_process_it
+    end
 end
