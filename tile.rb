18a69fd5e46e3938b4be95800c0b83ff23b600d7
rabbykhairul <rabbykhairul@gmail.com>
2019-09-14 08:44:31 +0600

    Add flagged attribute to tile class

--- tile.rb
+++ tile.rb
@@ -1,9 +1,10 @@
 class Tile
     attr_reader :bomb
     attr_accessor :visible
-    
+
     def initialize(bomb_indicator)
         @bomb = bomb_indicator
         @visible = false
+        @flagged = false
     end
 end
