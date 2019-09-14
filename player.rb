fd237c43ecca584955e8b99ab31e0beb21aeefcd
rabbykhairul <rabbykhairul@gmail.com>
2019-09-14 17:10:21 +0600

    Send command type inputted by player to game instance

--- player.rb
+++ player.rb
@@ -1,3 +1,11 @@
 class Player
-    
+    def initialize
+        @move_wish
+        @move_position
+    end
+
+    def get_move_wish
+        display_prompt_and_get_command
+        extract_move_wish_from_command
+    end
 end
